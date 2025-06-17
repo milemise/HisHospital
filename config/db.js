6837f52758d83aa5e06d67ef3a90f1ea7d5f3563
milemise <milivicmp@gmail.com>
2025-06-17 18:29:52 -0300

    db

--- config/db.js
+++ config/db.js
@@ -4,17 +4,8 @@ require("dotenv").config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, require: true
+    rejectUnauthorized: false
   }
-});
-
-pool.connect()
-.then(client => {
-console.log('Conexión exitosa a Neon');
-client.release();
-})
-.catch(err => {
-console.error('Error de conexión a Neon', err);
 });
 
 module.exports = pool;
