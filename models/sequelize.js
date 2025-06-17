3fa84af08ec73d961f4c90e0dff5df9a16fe70ff
milemise <milivicmp@gmail.com>
2025-06-17 19:46:46 -0300

    nuevo intento para vercel

--- models/sequelize.js
+++ models/sequelize.js
@@ -1,26 +1,27 @@
- require('dotenv').config();
- const { Sequelize } = require('sequelize');
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
- const sequelize = new Sequelize(
-   process.env.DB_NAME || 'neondb',
-   process.env.DB_USER || 'neondb_owner',
-   process.env.DB_PASSWORD || 'npg_5WnTyfGhgK0l',
-   {
-     host: process.env.DB_HOST || 'ep-odd-breeze-a4zvl9vk-pooler.us-east-1.aws.neon.tech',
-     dialect: 'postgres', 
-     logging: false,
-     define: {
-       freezeTableName: true,
-       underscored: true,
-       timestamps: true,
-       id: false
-     },
-     pool: {
-       max: 5,
-       min: 0,
-       acquire: 30000,
-       idle: 10000
-     }
-   }
- );
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres', 
+    logging: false,
+    define: {
+      freezeTableName: true,
+      underscored: true,
+      timestamps: true,
+      id: false
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
+
 module.exports = sequelize;
