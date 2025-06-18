557a56cf6f736d4b5078a9e266f1bb1bf2cc7283
milemise <milivicmp@gmail.com>
2025-06-17 23:06:32 -0300

    nuevo intento

--- models/sequelize.js
+++ models/sequelize.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'postgres', 
+    dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
     define: {
       freezeTableName: true,
       underscored: true,
